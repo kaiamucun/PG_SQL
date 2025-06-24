c830c87f3d5fa52d8fbd4ae9268356d389525600
kaia <kaiyamucun526@gmail.com>
2025-06-24 17:59:48 +0900

    まだ未作成のファイルに_を追加し

--- core/tournament_price.sql
+++ core/tournament_price.sql
@@ -20,6 +20,187 @@ CREATE TABLE tournament_price (
 
 START TRANSACTION;
 
+-- Tournament 1-15: メイントーナメント(詳細構造)
+-- Tournament 1: 昼ポーカー 2.5K
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(1,'entry','JPY',2500,15000,'昼ポーカー',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(1,'reentry','JPY',2500,15000,'リエントリー',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(1,'addon','JPY',2000,12000,'アドオン',1);
+
+-- Tournament 2: 夜ポーカー 3K
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(2,'entry','JPY',3000,20000,'夜ポーカー',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(2,'reentry','JPY',3000,20000,'リエントリー',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(2,'addon','JPY',2500,15000,'アドオン',1);
+
+-- Tournament 3: 深夜ポーカー 4K
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(3,'entry','JPY',4000,25000,'深夜ポーカー',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(3,'reentry','JPY',4000,25000,'リエントリー',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(3,'addon','JPY',3000,20000,'アドオン',1);
+
+-- Tournament 4: 週末スペシャル 5K
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(4,'entry','JPY',5000,30000,'週末スペシャル',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(4,'reentry','JPY',5000,30000,'リエントリー',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(4,'addon','JPY',4000,25000,'アドオン',1);
+
+-- Tournament 5: ディープスタック 6K
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(5,'entry','JPY',6000,40000,'ディープスタック',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(5,'reentry','JPY',6000,40000,'リエントリー',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(5,'addon','JPY',5000,30000,'アドオン',1);
+
+-- Tournament 6: ターボ 1K
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(6,'entry','JPY',1000,10000,'ターボ',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(6,'reentry','JPY',1000,10000,'リエントリー',1);
+
+-- Tournament 7: フリーズアウト 3.5K
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(7,'entry','JPY',3500,25000,'フリーズアウト',1);
+
+-- Tournament 8: Double Stack 4.5K
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(8,'entry','JPY',4500,50000,'ダブルスタック',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(8,'reentry','JPY',4500,50000,'リエントリー',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(8,'addon','JPY',3500,40000,'アドオン',1);
+
+-- Tournament 9: Super Turbo 800
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(9,'entry','JPY',800,8000,'スーパーターボ',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(9,'reentry','JPY',800,8000,'リエントリー',1);
+
+-- Tournament 10: Hyper Turbo 600
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(10,'entry','JPY',600,6000,'ハイパーターボ',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(10,'reentry','JPY',600,6000,'リエントリー',1);
+
+-- Tournament 11: Sunday Special 7K
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(11,'entry','JPY',7000,50000,'サンデースペシャル',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(11,'reentry','JPY',7000,50000,'リエントリー',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(11,'addon','JPY',5000,35000,'アドオン',1);
+
+-- Tournament 12: Knockout 2.8K
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(12,'entry','JPY',2800,15000,'ノックアウト',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(12,'reentry','JPY',2800,15000,'リエントリー',1);
+
+-- Tournament 13: Progressive KO 3.2K
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(13,'entry','JPY',3200,18000,'プログレッシブKO',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(13,'reentry','JPY',3200,18000,'リエントリー',1);
+
+-- Tournament 14: Satellite 1.5K
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(14,'entry','JPY',1500,12000,'サテライト',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(14,'reentry','JPY',1500,12000,'リエントリー',1);
+
+-- Tournament 15: Mystery Bounty 3.8K
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(15,'entry','JPY',3800,22000,'ミステリーバウンティ',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(15,'reentry','JPY',3800,22000,'リエントリー',1);
+
+-- Tournament 16-18: 早朝・モーニング系
+-- Tournament 16: 早朝ポーカー 1.2K
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(16,'entry','JPY',1200,10000,'早朝ポーカー',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(16,'reentry','JPY',1200,10000,'リエントリー',1);
+
+-- Tournament 17: Morning Glory 2K
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(17,'entry','JPY',2000,15000,'モーニンググローリー',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(17,'reentry','JPY',2000,15000,'リエントリー',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(17,'addon','JPY',1500,10000,'アドオン',1);
+
+-- Tournament 18: Morning Breaker 1.5K
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(18,'entry','JPY',1500,12000,'モーニングブレイカー',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(18,'reentry','JPY',1500,12000,'リエントリー',1);
+
+-- Tournament 19-20: 深夜高額系
+-- Tournament 19: High Roller Midnight 10K
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(19,'entry','JPY',10000,60000,'ハイローラー深夜',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(19,'reentry','JPY',10000,60000,'リエントリー',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(19,'addon','JPY',8000,50000,'アドオン',1);
+
+-- Tournament 20: Super High Roller 15K
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(20,'entry','JPY',15000,80000,'スーパーハイローラー',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(20,'reentry','JPY',15000,80000,'リエントリー',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(20,'addon','JPY',12000,60000,'アドオン',1);
+
+-- Tournament 21-22: 特殊系
+-- Tournament 21: Progressive Jackpot 2K
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(21,'entry','JPY',2000,15000,'プログレッシブJP',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(21,'reentry','JPY',2000,15000,'リエントリー',1);
+
+-- Tournament 22: Lightning Round 800
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(22,'entry','JPY',800,8000,'ライトニング',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(22,'reentry','JPY',800,8000,'リエントリー',1);
+
+-- Tournament 23-26: ミッドクイック系
+-- Tournament 23: Rush Poker 1.5K
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(23,'entry','JPY',1500,12000,'ラッシュポーカー',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(23,'reentry','JPY',1500,12000,'リエントリー',1);
+
+-- Tournament 24: Afternoon Quick 1K
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(24,'entry','JPY',1000,10000,'アフタヌーンクイック',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(24,'reentry','JPY',1000,10000,'リエントリー',1);
+
+-- Tournament 25: Evening Start 2K
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(25,'entry','JPY',2000,15000,'イブニングスタート',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(25,'reentry','JPY',2000,15000,'リエントリー',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(25,'addon','JPY',1500,10000,'アドオン',1);
+
+-- Tournament 26: Night Express 1.5K
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(26,'entry','JPY',1500,12000,'ナイトエクスプレス',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(26,'reentry','JPY',1500,12000,'リエントリー',1);
+
+-- Tournament 27-30: 中級系
+-- Tournament 27: Weekend Warrior 2.5K
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(27,'entry','JPY',2500,18000,'ウィークエンドウォリアー',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(27,'reentry','JPY',2500,18000,'リエントリー',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(27,'addon','JPY',2000,15000,'アドオン',1);
+
+-- Tournament 28: Happy Hour 1.8K
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(28,'entry','JPY',1800,14000,'ハッピーアワー',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(28,'reentry','JPY',1800,14000,'リエントリー',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(28,'addon','JPY',1500,12000,'アドオン',1);
+
+-- Tournament 29: Deep Night 5K
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(29,'entry','JPY',5000,35000,'ディープナイト',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(29,'reentry','JPY',5000,35000,'リエントリー',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(29,'addon','JPY',4000,30000,'アドオン',1);
+
+-- Tournament 30: Ultra Deep 8K
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(30,'entry','JPY',8000,50000,'ウルトラディープ',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(30,'reentry','JPY',8000,50000,'リエントリー',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(30,'addon','JPY',6000,40000,'アドオン',1);
+
+-- Tournament 31-40: 基本系
+-- Tournament 31: Rapid Fire 800
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(31,'entry','JPY',800,8000,'ラピッドファイア',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(31,'reentry','JPY',800,8000,'リエントリー',1);
+
+-- Tournament 32: Speed Play 600
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(32,'entry','JPY',600,6000,'スピードプレイ',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(32,'reentry','JPY',600,6000,'リエントリー',1);
+
+-- Tournament 33: Power Play 1.2K
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(33,'entry','JPY',1200,10000,'パワープレイ',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(33,'reentry','JPY',1200,10000,'リエントリー',1);
+
+-- Tournament 34: Blitz 900
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(34,'entry','JPY',900,9000,'ブリッツ',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(34,'reentry','JPY',900,9000,'リエントリー',1);
+
+-- Tournament 35: Flash 700
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(35,'entry','JPY',700,7000,'フラッシュ',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(35,'reentry','JPY',700,7000,'リエントリー',1);
+
+-- Tournament 36: Quick Strike 1.1K
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(36,'entry','JPY',1100,10000,'クイックストライク',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(36,'reentry','JPY',1100,10000,'リエントリー',1);
+
+-- Tournament 37: Express 1.3K
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(37,'entry','JPY',1300,11000,'エクスプレス',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(37,'reentry','JPY',1300,11000,'リエントリー',1);
+
+-- Tournament 38: Lightning 800
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(38,'entry','JPY',800,8000,'ライトニング',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(38,'reentry','JPY',800,8000,'リエントリー',1);
+
+-- Tournament 39: Burst 1K
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(39,'entry','JPY',1000,10000,'バースト',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(39,'reentry','JPY',1000,10000,'リエントリー',1);
+
+-- Tournament 40: Ultra Dash 1.5K
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(40,'entry','JPY',1500,12000,'ウルトラダッシュ',1);
+INSERT INTO tournament_price(tournament_no,price_type,unit,price,stack,remark,enabled) VALUES(40,'reentry','JPY',1500,12000,'リエントリー',1);
+
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
