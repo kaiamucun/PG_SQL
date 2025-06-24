1edb199bf44b166ef5cfad519363596caaddb1e2
kaia <kaiyamucun526@gmail.com>
2025-06-24 10:59:49 +0900

    admin_userを各roomごとに作成した

--- core/admin_user_org.sql
+++ core/admin_user_org.sql
@@ -19,13 +19,27 @@ START TRANSACTION;
 
 INSERT INTO admin_user_org (user_id, org_no, permission_bit_flag) VALUES ('master', 1, 929);
 
-INSERT INTO admin_user_org (user_id, org_no, permission_bit_flag) SELECT 'room_admin_01', org_no, 122 FROM org WHERE org_type = 'room' AND enabled = 1;
-
-INSERT INTO admin_user_org (user_id, org_no, permission_bit_flag) SELECT 'room_admin_02', org_no, 122 FROM org WHERE org_type = 'room' AND enabled = 1;
-
-INSERT INTO admin_user_org (user_id, org_no, permission_bit_flag) SELECT 'room_admin_03', org_no, 122 FROM org WHERE org_type = 'room' AND enabled = 1;
-
-COMMIT;
+-- 各roomごとの管理者を作成（org_no 2-21に対応）
+INSERT INTO admin_user_org (user_id, org_no, permission_bit_flag) VALUES ('room_admin_01', 2, 122);   -- AG
+INSERT INTO admin_user_org (user_id, org_no, permission_bit_flag) VALUES ('room_admin_02', 3, 122);   -- IG
+INSERT INTO admin_user_org (user_id, org_no, permission_bit_flag) VALUES ('room_admin_03', 4, 122);  -- QC
+INSERT INTO admin_user_org (user_id, org_no, permission_bit_flag) VALUES ('room_admin_04', 5, 122);  -- GGSJ
+INSERT INTO admin_user_org (user_id, org_no, permission_bit_flag) VALUES ('room_admin_05', 6, 122);   -- GGSB
+INSERT INTO admin_user_org (user_id, org_no, permission_bit_flag) VALUES ('room_admin_06', 7, 122);   -- GGNG
+INSERT INTO admin_user_org (user_id, org_no, permission_bit_flag) VALUES ('room_admin_07', 8, 122);   -- GGOS
+INSERT INTO admin_user_org (user_id, org_no, permission_bit_flag) VALUES ('room_admin_08', 9, 122);   -- KKSB
+INSERT INTO admin_user_org (user_id, org_no, permission_bit_flag) VALUES ('room_admin_09', 10, 122);  -- KKSJ
+INSERT INTO admin_user_org (user_id, org_no, permission_bit_flag) VALUES ('room_admin_10', 11, 122);  -- SAP
+INSERT INTO admin_user_org (user_id, org_no, permission_bit_flag) VALUES ('room_admin_11', 12, 122);  -- CLT
+INSERT INTO admin_user_org (user_id, org_no, permission_bit_flag) VALUES ('room_admin_12', 13, 122);  -- NG
+INSERT INTO admin_user_org (user_id, org_no, permission_bit_flag) VALUES ('room_admin_13', 14, 122);  -- Rela
+INSERT INTO admin_user_org (user_id, org_no, permission_bit_flag) VALUES ('room_admin_14', 15, 122);  -- NCA
+INSERT INTO admin_user_org (user_id, org_no, permission_bit_flag) VALUES ('room_admin_15', 16, 122);  -- Gut
+INSERT INTO admin_user_org (user_id, org_no, permission_bit_flag) VALUES ('room_admin_16', 17, 122);  -- RGS
+INSERT INTO admin_user_org (user_id, org_no, permission_bit_flag) VALUES ('room_admin_17', 18, 122);  -- NCS
+INSERT INTO admin_user_org (user_id, org_no, permission_bit_flag) VALUES ('room_admin_18', 19, 122);  -- 8YH
+INSERT INTO admin_user_org (user_id, org_no, permission_bit_flag) VALUES ('room_admin_19', 20, 122);  -- BBEB
+INSERT INTO admin_user_org (user_id, org_no, permission_bit_flag) VALUES ('room_admin_20', 21, 122);  -- BKNK
 
 COMMIT;
 
